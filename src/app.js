a213b9cf05fd8a13fcd987b53fae356fcd888aad
Matthew Giles <matty92g@hotmail.co.uk>
2023-03-01 18:08:37 +0000

    Set Up 8

--- src/app.js
+++ src/app.js
@@ -1,15 +1,10 @@
 const express = require('express');
-// Import your artistRouter into app.js and direct all /artists to your artistController.
-const { artistRouter } = require('./routes/artist');
+const artistRouter = require('./routes/artist');
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/artists', artistRouter);
-
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
 
 module.exports = app;
--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -1,6 +1,18 @@
-// Create a src/controllers/artist.js file and write a controller function which returns a 201 status code.
-const createArtist = (req, res) => {
-  res.sendStatus(201);
+const db = require('../db/index.js');
+const createArtist = async (req, res) => {
+  const { name, genre } = req.body;
+
+  try {
+    const {
+      rows: [artist],
+    } = await db.query(
+      'INSERT INTO Artists (name, genre) VALUES ($1, $2) RETURNING *',
+      [name, genre]
+    );
+    res.status(201).json(artist);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
 };
 
 module.exports = { createArtist };
