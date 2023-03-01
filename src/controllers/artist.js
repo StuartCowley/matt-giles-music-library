300d0fa4b0ef7d8c400737709e770ecf16b62e86
Matthew Giles <matty92g@hotmail.co.uk>
2023-03-01 21:56:58 +0000

    Able to Delete Artist with ID in request parameters

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -11,6 +11,7 @@ const createArtist = async (req, res) => {
     );
     res.status(201).json(artist);
   } catch (err) {
+    console.log(err);
     res.status(500).json(err.message);
   }
 };
@@ -20,6 +21,7 @@ const getAllArtists = async (req, res) => {
     const { rows } = await db.query('SELECT * FROM Artists');
     res.status(200).json(rows);
   } catch (err) {
+    console.log(err);
     res.status(500).json(err.message);
   }
 };
@@ -35,8 +37,59 @@ const getArtistById = async (req, res) => {
     }
     res.status(200).json(artist);
   } catch (err) {
+    console.log(err);
     res.status(500).json(err.message);
   }
 };
 
-module.exports = { createArtist, getAllArtists, getArtistById };
+const updateArtist = async (req, res) => {
+  const { id } = req.params;
+  const { name, genre } = req.body;
+  let query, params;
+  if (name && genre) {
+    query = `UPDATE Artists SET name = $1, genre = $2 WHERE id = $3 RETURNING *`;
+    params = [name, genre, id];
+  } else if (name) {
+    query = `UPDATE Artists SET name = $1 WHERE id = $2 RETURNING *`;
+    params = [name, id];
+  } else if (genre) {
+    query = `UPDATE Artists SET genre = $1 WHERE id = $2 RETURNING *`;
+    params = [genre, id];
+  }
+  try {
+    const {
+      rows: [artist],
+    } = await db.query(query, params);
+    if (!artist) {
+      return res.status(404).json({ message: `artist ${id} does not exist` });
+    }
+    res.status(200).json(artist);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err.message);
+  }
+};
+
+const deleteArtist = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      rows: [artist],
+    } = await db.query('DELETE FROM Artists WHERE id = $1 RETURNING *', [id]);
+    if (!artist) {
+      return res.status(404).json({ message: `artist ${id} does not exist` });
+    }
+    res.status(200).json(artist);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err.message);
+  }
+};
+
+module.exports = {
+  createArtist,
+  getAllArtists,
+  getArtistById,
+  updateArtist,
+  deleteArtist,
+};
--- src/routes/artist.js
+++ src/routes/artist.js
@@ -6,5 +6,7 @@ const router = express.Router();
 router.post('/', artistController.createArtist);
 router.get('/', artistController.getAllArtists);
 router.get('/:id', artistController.getArtistById);
+router.patch('/:id', artistController.updateArtist);
+router.delete('/:id', artistController.deleteArtist);
 
 module.exports = router;
